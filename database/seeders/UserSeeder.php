7425f68dead1b4223001e37aea9fa6346e49d09d
tecdesoftware777 <ieuan0911@gmail.com>
2026-03-03 16:26:03 -0600

    Refactor `User::firstOrCreate` call in UserSeeder to explicitly separate find and create attributes.

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,15 +14,16 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         //Crea un usuario de prueba cada que ejecuto migrations
-        $doctor = User::firstOrCreate([
-            'email' => 'luis@example.com',
-            'name' => 'Luis Vera',
-            'password' => bcrypt('12345678'),
-            'id_number' => '123456789',
-            'phone' => '7777777777',
-            'address' => 'Calle 123, Gran Santa Fe',
-            ]);
-            
+        $doctor = User::firstOrCreate(
+            ['email' => 'luis@example.com', 'id_number' => '123456789'],
+            [
+                'name' => 'Luis Vera',
+                'password' => bcrypt('12345678'),
+                'phone' => '7777777777',
+                'address' => 'Calle 123, Gran Santa Fe',
+            ]
+        );
+
         $doctor->assignRole('Doctor');
         if (!$doctor->doctor) {
             $doctor->doctor()->create();
